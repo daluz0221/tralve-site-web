282cb59e4ae5a8a306f246ec7f909bc22525b10c
Luis Felipe <lfelipe.desarrollador@gmail.com>
2021-09-18 15:48:54 -0500

    proyecto terminado

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ let formBtn = document.querySelector("#login-btn");
 let loginForm = document.querySelector(".login-form-container");
 let formClose = document.querySelector("#form-close");
 let menu = document.querySelector('#menu-bar');  
-let navbar = document.querySelector('.navbar');  
+let navbar = document.querySelector('.navbar'); 
+let videoBtn = document.querySelectorAll('.vid-btn'); 
+
 
 
 window.onscroll = () =>{
@@ -35,3 +37,28 @@ formBtn.addEventListener('click', ()=>{
 formClose.addEventListener('click', ()=>{
     loginForm.classList.remove('active');
 });
+
+
+videoBtn.forEach(btn=>{
+     btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+      
+     });
+});
+
+
+// var swiper = new Swiper(".review-slider", {
+//     spaceBetween: 20,
+//     loop: true,
+// });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+});
+
+
+
